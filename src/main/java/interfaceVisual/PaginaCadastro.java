d12c72137e26f08312cadc10f2565fc262d11a31
CaioXimendes <caioximendes1@gmail.com>
2024-11-16 10:51:38 -0300

    Creating registering visual interface

---
+++ src/main/java/interfaceVisual/PaginaCadastro.java
@@ -0,0 +1,9 @@
+package interfaceVisual;
+
+import javax.swing.*;
+
+public class PaginaCadastro {
+    private JTextField insiraOSeuNomeTextField;
+    private JTextField insiraOSeuEmailTextField;
+    private JPasswordField exemploPasswordField;
+}
