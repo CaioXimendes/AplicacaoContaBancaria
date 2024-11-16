1264ad7b5d0396890e0d6168875897ead930a119
CaioXimendes <caioximendes1@gmail.com>
2024-11-16 18:28:59 -0300

    adding main page

---
+++ src/main/java/interfaceVisual/PaginaPrincipal.java
@@ -0,0 +1,14 @@
+package interfaceVisual;
+
+import javax.swing.*;
+
+public class PaginaPrincipal {
+    private JTextArea texto1TextArea;
+    private JTextArea texto2TextArea;
+    private JTextArea valorSaldoTextArea;
+    private JTextArea valorDepositoTextArea;
+    private JTextArea valorTransferenciaTextArea;
+    private JTextArea numeroTextArea;
+    private JButton transferirButton;
+    private JButton sairButton;
+}
