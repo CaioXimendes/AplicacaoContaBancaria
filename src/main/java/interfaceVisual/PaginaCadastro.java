69b2dfc4632a831c792ca057e2c31b5e68d80d68
CaioXimendes <caioximendes1@gmail.com>
2024-11-16 11:57:37 -0300

    adding cadastrarButton and testing if it works

--- src/main/java/interfaceVisual/PaginaCadastro.java
+++ src/main/java/interfaceVisual/PaginaCadastro.java
@@ -1,9 +1,31 @@
 package interfaceVisual;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PaginaCadastro {
-    private JTextField insiraOSeuNomeTextField;
-    private JTextField insiraOSeuEmailTextField;
-    private JPasswordField exemploPasswordField;
+public class PaginaCadastro extends JFrame{
+    private JTextField nomeTextField;
+    private JTextField emailTextField;
+    private JPasswordField passwordField;
+    private JButton cadastrarButton;
+    private JPanel JPanelMain;
+
+    public PaginaCadastro(){
+
+        cadastrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Cadastrado!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        PaginaCadastro p1 = new PaginaCadastro();
+        p1.setContentPane(p1.JPanelMain);
+        p1.setVisible(true);
+        p1.setSize(500,500);
+        p1.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
