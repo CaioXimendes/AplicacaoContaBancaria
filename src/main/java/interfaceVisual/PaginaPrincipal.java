e786977d2c5502fed9f35739b9ab5e7f2497e677
CaioXimendes <caioximendes1@gmail.com>
2024-11-17 07:54:35 -0300

    adding exit deposit and transfer button

--- src/main/java/interfaceVisual/PaginaPrincipal.java
+++ src/main/java/interfaceVisual/PaginaPrincipal.java
@@ -1,8 +1,10 @@
 package interfaceVisual;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PaginaPrincipal {
+public class PaginaPrincipal extends JFrame{
     private JTextArea texto1TextArea;
     private JTextArea texto2TextArea;
     private JTextArea valorSaldoTextArea;
@@ -11,4 +13,31 @@ public class PaginaPrincipal {
     private JTextArea numeroTextArea;
     private JButton transferirButton;
     private JButton sairButton;
+    private JPanel painelPrincipal;
+    private JButton depositarButton;
+
+    public PaginaPrincipal(){
+        setContentPane(painelPrincipal);
+        transferirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: atualizar os valores de saldo apos realizar uma transferencia
+
+            }
+        });
+
+        depositarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: atualizar os valores de saldo apos realizar um deposito
+            }
+        });
+        sairButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: fechar a pagina apos apertar o botao de sair
+                dispose();
+            }
+        });
+    }
 }
